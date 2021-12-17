4361b9835423ce7143e6f8df646ec818b4eddab0
Jon Herbst <herbtaculargames@gmail.com>
2021-12-17 17:19:35 +0000

    Begin class setup and size testing of ASCII art representations

--- generator/image.py
+++ generator/image.py
@@ -1,1 +1,14 @@
-"""Handle construction of ASCII art squares."""
+class ImageGen:
+    """
+    Handles the construction of the ASCII art
+    rectangles that represent what software,
+    styles and influences should be used
+    for the next project.
+    """
+    def __init__(self):
+        return
+
+    def getBase(self):
+        return [
+            ['+', '-', '-', '-', '-', '-', '-', '-', '-', '-', '+']
+        ]
